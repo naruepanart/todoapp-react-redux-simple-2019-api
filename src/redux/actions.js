47c30a66fd66b15f162e55d95eb0e70506a33869
Naruepanart Siangsanan <naruepanartwork@gmail.com>
2019-10-10 19:03:46 +0700

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-export const firstName = e => async dispatch => {
-  dispatch({ type: "FIRST_NAME", payload: e.target.value });
-};
-
-export const reSetForm = () => async dispatch => {
-  dispatch({ type: "FORM_RESET", payload: "" });
-};
-
 export const fetchProfile = () => async dispatch => {
   const res = await axios.get("http://localhost:3001/profile");
   dispatch({ type: "FETCH_PROFILE", payload: res.data });
